564cc42d9d89177cd2d65e632dd08d70e2cefdc5
Saminwza007 <s6452410016@sau.ac.th>
2023-08-09 16:22:11 +0700

    V.1

---
+++ test01.dart
@@ -0,0 +1,31 @@
+/*
+
+ *  Tham
+
+ *  Orf: DTI-sau
+
+ *
+
+ * */
+
+main() {
+  //string -> "?????" , '???????'
+
+  print("xxxx\nx");
+
+  print('YY\tYYY');
+
+  //number -> integer , double
+
+  print(1150);
+
+  print(29.25589);
+
+  print('1212');
+
+  //boolean -> true, false
+
+  print(true);
+
+  print(false);
+}
---
+++ test05.dart
@@ -0,0 +1,20 @@
+main() {
+  print('10 + 20 + 10');
+  print('S{10 + 20 + 10}');
+
+  String a = "AAAA";
+  String b = "BBBB";
+  int x = 20;
+  int y = 30;
+  double z = 123.456;
+  var num1 = 555;
+
+  print("Hell...");
+  print(a + b + 'Wow wow wow');
+  print('${a}');
+  print(a);
+  print(y + x);
+
+  print('${x} Hi....');
+  //ฟังก์ฃั่น-เมธอด การทำงานเกี่ยวกับตัวเลข
+}
